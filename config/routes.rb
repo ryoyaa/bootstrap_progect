5f18e074aeee58acb4ffe4642ff4d48aa93d40fb
ryoyaa <ryoya76lima@gmail.com>
2018-05-02 13:05:25 +0900

    bootstrap使用

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'samples/index'
+  root 'samples#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
